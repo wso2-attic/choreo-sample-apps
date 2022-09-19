8baa650fece25aa2c424066204cfc9673471c640
Tharindu Muhandiram <tnnmuhandiram@gmail.com>
2022-09-19 08:54:32 +0530

    adding java rest api sample

---
+++ java/rest-api/src/main/java/wso2/example/byoc/ByocApplication.java
@@ -0,0 +1,13 @@
+package wso2.example.byoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByocApplication.class, args);
+	}
+
+}
