747701ee5474a54fdb9820c28a5a959489a50637
Charith Wickramasinghe <charith.w10@gmail.com>
2022-09-19 11:13:49 +0530

    dotnet refactor

---
+++ dotnet/rest-api/weather-api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using weather_api.Services;
+
+namespace weather_api.Controllers;
+
+[ApiController]
+[Route("weather")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IMemoryCache _memoryCache;
+    private IWeatherService weatherService;
+
+    private readonly string cacheKey = "weatherapiKey";
+
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,
+        IMemoryCache memoryCache)
+    {
+        _logger = logger;
+        _memoryCache = memoryCache;
+        weatherService = new WeatherService();
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        IEnumerable<WeatherForecast>? weatherForecastCollection = null;
+
+        if (_memoryCache.TryGetValue(cacheKey, out weatherForecastCollection))
+        {
+            return weatherForecastCollection;
+        }
+
+        weatherForecastCollection = weatherService.GetWeather(Summaries);
+
+        var cacheOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromSeconds(10))
+            .SetAbsoluteExpiration(TimeSpan.FromSeconds(30));
+
+        _memoryCache.Set(cacheKey, weatherForecastCollection, cacheOptions);
+        return weatherForecastCollection;
+    }
+
+}
+
---
+++ dotnet/rest-api/weather-api/Services/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace weather_api.Services
+{
+    public interface IWeatherService
+    {
+        IEnumerable<WeatherForecast> GetWeather(string[] summaries);
+    }
+}
+
