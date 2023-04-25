0662892d0e087f60f478e03342014e3f181d058b
janithrs <janitravindu@gmail.com>
2023-04-25 16:26:10 +0530

    Add endpoints.yaml
    remove old app mjs
    
    add new methods for book mgt
    
    update endpoint yaml
    
    update open api spec
    
    update api spec
    
    update ep name
    
    remove isbn and set auto uuid
    
    enable set status
    
    update comment
    
    update path
    
    update yaml
    
    update endpoint yaml
    
    update pkg.json
    
    update ep yaml
    
    remove ttl
    
    remove ttl cache
    
    update ttl
    
    update comments

--- nodejs/rest-api/app.mjs
+++ nodejs/rest-api/app.mjs
@@ -1,47 +1,86 @@
 import express from "express";
 import cache from "./cache.mjs";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/keys/:key", (req, res) => {
-  const key = req.params.key;
-  if (!key || typeof key !== "string") {
-    return res.status(400).json({ error: "missing or invalid key" });
+// add a book - request body should contain a title, status and an author
+app.post("/reading-list/books", (req, res) => {
+  const { title, author, status } = req.body;
+  const uuid = uuidv4();
+  if (!(status === "read" || status === "to_read" || status === "reading")) {
+    return res.status(400).json({
+      error: "Status is invalid. Accepted statuses: read | to_read | reading",
+    });
   }
-  if (!cache.has(key)) {
-    return res.status(404).json({ error: "key does not exist" });
+  if (!title || !author || !status) {
+    return res.status(400).json({ error: "Title, Status or Author is empty" });
   }
-  const value = cache.get(key);
-  return res.json({ key: id, value });
+  const value = { uuid, title, author, status };
+  cache.set(uuid, value, 86400);
+  return res.status(201).json({ uuid, title, author });
 });
 
-app.get("/api/keys", (_, res) => {
-  return res.json(cache.keys());
+// update status of a book by uuid
+app.put("/reading-list/books/:uuid", (req, res) => {
+  const uuid = req.params.uuid;
+  const { status } = req.body;
+  if (!uuid || typeof uuid !== "string") {
+    return res.status(400).json({ error: "missing or invalid UUID" });
+  }
+  if (!cache.has(uuid)) {
+    return res.status(404).json({ error: "UUID does not exist" });
+  }
+  if (!(status === "read" || status === "to_read" || status === "reading")) {
+    return res.status(400).json({
+      error: "Status is invalid. Accepted statuses: read | to_read | reading",
+    });
+  }
+  const value = cache.get(uuid);
+  value.status = status;
+  cache.set(uuid, value);
+  return res.json({ uuid, status });
 });
 
-app.post("/api/keys", (req, res) => {
-  const { key, value, ttl } = req.body;
-  if (!key || !value) {
-    return res.status(400).json({ error: "invalid request body" });
+// get the list of books
+app.get("/reading-list/books", (_, res) => {
+  const keys = cache.keys();
+  const allData = {};
+  for (const key of keys) {
+    allData[key] = cache.get(key);
   }
-  cache.set(key, value, ttl ?? 0);
-  return res.status(201).json({ key });
+  return res.json(allData);
 });
 
-app.delete("/api/keys/:key", (req, res) => {
-  const key = req.params.key;
-  if (!key || typeof key !== "string") {
-    return res.status(400).json({ error: "missing or invalid key" });
+// get a book by uuid
+app.get("/reading-list/books/:uuid", (req, res) => {
+  const uuid = req.params.uuid;
+  if (!uuid || typeof uuid !== "string") {
+    return res.status(400).json({ error: "missing or invalid UUID" });
   }
-  if (!cache.has(key)) {
-    return res.status(404).json({ error: "key does not exist" });
+  if (!cache.has(uuid)) {
+    return res.status(404).json({ error: "UUID does not exist" });
   }
-  cache.del(key);
-  return res.json({ key });
+  const value = cache.get(uuid);
+  return res.json(value);
 });
 
+// delete a book by uuid
+app.delete("/reading-list/books/:uuid", (req, res) => {
+  const uuid = req.params.uuid;
+  if (!uuid || typeof uuid !== "string") {
+    return res.status(400).json({ error: "missing or invalid UUID" });
+  }
+  if (!cache.has(uuid)) {
+    return res.status(404).json({ error: "UUID does not exist" });
+  }
+  cache.del(uuid);
+  return res.json({ uuid });
+});
+
+// health check
 app.get("/healthz", (_, res) => {
   return res.sendStatus(200);
 });
