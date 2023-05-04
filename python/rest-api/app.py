dd43547640e2ce8b005634168539fde94a0d80a4
janithrs <janithravindu@gmail.com>
2023-05-04 07:49:22 +0530

    setup python app

--- python/rest-api/app.py
+++ python/rest-api/app.py
@@ -1,40 +1,55 @@
-import os
-import random
-from flask import Flask
-from datetime import datetime, date, timedelta
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"]
-location = [ "Colombo", "Galle", "Kandy", "Jaffna", "Anuradhapura", "Trincomalee", "Negombo", "Ratnapura", "Matale"]
+books = []
 
-@app.route('/')
-def getWeatherApi():
-    return {"app":"weather-api", "time":datetime.now()};
+# helper function to generate IDs
+def generate_id():
+    return len(books) + 1
 
-@app.route('/healthz')
-def healthz():
-    return {"app":"weather-api", "time":datetime.now(), "status":"running"};
+# endpoint to get all books
+@app.route('/reading-list/books', methods=['GET'])
+def get_books():
+    return jsonify({'books': books})
 
-@app.route('/weather')
-def getWeatherForcast():
-    weatherResults = []
-    current_date = datetime.today()
+# endpoint to add a new book
+@app.route('/reading-list/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    book = {
+        'id': generate_id(),
+        'author': data['author'],
+        'name': data['name'],
+        'status': data['status']
+    }
+    books.append(book)
+    return jsonify(book)
 
-    for idx, s in enumerate(summaries):
-        tempC = random.randint(-20, 55)
-        print(idx)
-        weatherResults.append({
-            "date": current_date + timedelta(idx),
-            "temperatureC":tempC,
-            "temperatureF":32 + (int)(tempC / 0.5556),
-            "summary":s,
-            "location":location[idx]
-        })
-    
-    return weatherResults;
+# endpoint to get a book by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            return jsonify(book)
+    return '', 404
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+# endpoint to delete a book by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    global books
+    books = [book for book in books if book['id'] != book_id]
+    return '', 204
+
+# endpoint to update a book status by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['PUT'])
+def update_book_status(book_id):
+    data = request.get_json()
+    for book in books:
+        if book['id'] == book_id:
+            book['status'] = data['status']
+            return jsonify(book)
+    return '', 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
