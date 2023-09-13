49770ff446483f17b7b86d856fbcfd4da433ba8a
yashodgayashan <yashodgayashan@gmail.com>
2023-09-13 19:39:45 +0530

    update angular SPA

--- go/random-user-generation/go.mod
+++ go/random-user-generation/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/bxcodec/faker/v3 v3.8.1 // indirect
+	github.com/bxcodec/faker/v4 v4.0.0-beta.3 // indirect
 	github.com/go-faker/faker/v4 v4.1.1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- web-apps/angular-spa/src/app/app.module.ts
+++ web-apps/angular-spa/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
